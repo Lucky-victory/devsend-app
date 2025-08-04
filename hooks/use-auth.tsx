f91ffc9e606f929f07643760a4764975702e3d45
Lucky Victory <luckyvictory54@gmail.com>
2025-08-04 11:34:47 +0100

    Add debug logging in useAuth hook to track user authentication state and loading status

--- hooks/use-auth.tsx
+++ hooks/use-auth.tsx
@@ -4,6 +4,12 @@ import { useConvexAuth, useQuery } from "convex/react";
 export const useAuth = () => {
   const { isAuthenticated, isLoading } = useConvexAuth();
   const user = useQuery(api.auth.getCurrentUser);
+  console.log({
+    user,
+    isAuthenticated,
+    isLoading,
+  });
+
   const currentWorkspace = useQuery(api.auth.getCurrentWorkspace, {
     userId: user?._id,
   });
