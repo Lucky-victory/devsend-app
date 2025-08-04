2ce9d29db397b8cf7f401a749eccefe964bca3ec
Lucky Victory <luckyvictory54@gmail.com>
2025-08-04 10:07:44 +0100

    Refactor authentication API configuration to use updated environment variable for Convex site URL; remove legacy login and signup pages to streamline authentication process.

--- lib/auth.ts
+++ lib/auth.ts
@@ -18,7 +18,7 @@ import { betterAuthComponent } from "../convex/auth";
 import { requireMutationCtx } from "@convex-dev/better-auth/utils";
 import { GenericCtx } from "../convex/_generated/server";
 
-const siteUrl = "http://localhost:3201";
+const siteUrl = process.env.CONVEX_SITE_URL || "http://localhost:3201";
 // Split out options so they can be passed to the convex plugin
 const createOptions = (ctx: GenericCtx) =>
   ({
